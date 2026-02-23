70b745db6dc4de4d06350b7ab451632741b924c8
acliang1412 <acliang7229@gmail.com>
2026-02-23 17:43:11 -0600

    Testing out operations

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -5,9 +5,25 @@ SET @myVariable = 10
 PRINT @myVariable
 
 DECLARE @var1 INT, @var2 INT
-SET @var1 = 5
-SET @var2 = 10
+SET @var1 = 3
+SET @var2 = 5
 
-PRINT 'having fun wth' + ' TSQL and MS SQL Server'
+PRINT 'Variable 1 = ' + CONVERT(varchar(5), @var1) + CHAR(13) + 'Variable 2 = ' + CONVERT(varchar(5), @var2) + CHAR(13) + 'Total: '
+PRINT @var1 + @var2
 
-PRINT 'I Have $' + CONVERT(VARCHAR(50), @var1) + ' dollars '
+IF @var1 <= 3
+	BEGIN
+		PRINT 'Variable 1 <= '+ CONVERT(VARCHAR(5), @var1) + char(13)
+	END
+ELSE
+	BEGIN
+		PRINT 'Variable 1 is not < ' + CONVERT(varchar(5), @var1) + char(13)
+	END
+IF @var1 != 3
+	BEGIN
+		PRINT 'Variable 1 is NOT ' + CONVERT(varchar(5), @var1) + char(13)
+	END
+ELSE 
+	BEGIN
+		PRINT 'Variable 1 is not < 3' + CONVERT(varchar(5), @var1) + char(13)
+	END
