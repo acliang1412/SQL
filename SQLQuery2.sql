fe7d8178060caf6861400859885e3182e0fc6495
acliang1412 <acliang7229@gmail.com>
2026-02-23 17:46:50 -0600

    Testing different kind of operations .using IF/ELSE and AND

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -5,25 +5,25 @@ SET @myVariable = 10
 PRINT @myVariable
 
 DECLARE @var1 INT, @var2 INT
-SET @var1 = 3
+SET @var1 = 2
 SET @var2 = 5
 
 PRINT 'Variable 1 = ' + CONVERT(varchar(5), @var1) + CHAR(13) + 'Variable 2 = ' + CONVERT(varchar(5), @var2) + CHAR(13) + 'Total: '
 PRINT @var1 + @var2
 
-IF @var1 <= 3
+IF @var1 <  2
 	BEGIN
-		PRINT 'Variable 1 <= '+ CONVERT(VARCHAR(5), @var1) + char(13)
+		PRINT '@var1 1 < 2'
 	END
-ELSE
+ELSE IF @var1 > 1 AND @var1 < 3
 	BEGIN
-		PRINT 'Variable 1 is not < ' + CONVERT(varchar(5), @var1) + char(13)
+		PRINT '@var1 > 1 AND @var1 < 3' 
 	END
-IF @var1 != 3
+ELSE IF @var1 = 4 OR @var1 < 6
 	BEGIN
-		PRINT 'Variable 1 is NOT ' + CONVERT(varchar(5), @var1) + char(13)
+		PRINT '@var1 = 4 OR @var1 < 6' 
 	END
 ELSE 
 	BEGIN
-		PRINT 'Variable 1 is not < 3' + CONVERT(varchar(5), @var1) + char(13)
+		PRINT '@var1 does not qualify' 
 	END
